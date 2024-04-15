706de9464f97f330f85055a366007005cf7ddfed
Olga Genier <genier.developer@gmail.com>
2024-04-15 12:02:34 +0500

    delete contacts

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {Main} from "./layout/section/main/Main";
 import {Skills} from "./layout/section/skills/Skills";
 import {Languages} from "./layout/section/languages/Languages";
 import {Works} from "./layout/section/works/Works";
-import {Contact} from "./layout/section/contacts/Contact";
 import {Footer} from "./layout/footer/Footer";
 import {GoTopBtn} from "./components/goTopBtn/GoTopBtn";
 
